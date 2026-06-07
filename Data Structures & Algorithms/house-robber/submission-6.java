0644701cd2058dd6cc83eb602bf479d959a2f428
AnikS0498 <54571594+AnikS0498@users.noreply.github.com>
2026-06-08 00:16:57 +0530

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int l = nums.length;
+        int[] res = new int[l];
+
+        if (l < 2) {
+            return nums[0];
+        }
+
+        res[0] = nums[0];
+        res[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < l; i++) {
+            res[i] = Math.max(res[i - 2] + nums[i], res[i - 1]);
+        }
+
+        return res[l - 1];
+    }
+}
